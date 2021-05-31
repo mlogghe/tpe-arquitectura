a074dbdba18e31e6c60af485b13ff84d34949897
Fran <fkoenigberengualumnos.exa.unicen.edu.ar>
2021-05-31 10:58:08 -0300

    1ra Etapa - Actualizacion

--- processor.vhd
+++ processor.vhd
@@ -24,69 +24,56 @@ end processor;
 
 architecture processor_arq of processor is 
 	
-    type reg is array(31 downto 0) of STD_LOGIC;
-    signal if_pc_out: reg;      --Salida PC
-    signal if_pc_in: reg;       --Entrada PC
+    signal if_pc_out: std_logic_vector(31 downto 0);        --Salida PC
     
-    signal DataOut: reg;        --Salida instruction memory
-    signal Adress: reg;         --Entrada instruction memory (PC)
+    signal if_pc_4: std_logic_vector(31 downto 0);          --Salida Adder
     
-    signal if_sig: reg;         --Salida Add
+    signal mem_branch: std_logic_vector(31 downto 0);       --Salto proveniente de etapa mem
     
-    signal mem_branch: reg;     --Salto proveniente de etapa mem
+    signal if_sel_mux: std_logic;                           --Selector mux
+    signal if_mux_out: std_logic_vector(31 downto 0);       --Salida mux
     
-    signal sel_mux: std_logic;  --Selector mux
-    signal out_mux: reg;        --Salida mux
-    
-    signal if_id_pc_4_in: reg;  --Entrada registro segmentacion para Adder
-    signal if_id_inst_in: reg;  --Entrada registro segmentacion para Instrucc mem
+    signal if_id_pc_4_out: std_logic_vector(31 downto 0);   --Salida registro segmentacion para Adder
+    signal if_id_inst_out: std_logic_vector(31 downto 0);   --Salida registro segmentacion para Instrucc mem
     
 begin 
 
 	--Primera Etapa
+	
 	--PC
     Reg_pc: process(Clk, Reset)
     begin
     	if (Reset = '1') then
-    	   if_pc_out <= (others => '0');    --Salida del PC en 0
+    	   if_pc_out <= (others => '0');       --Salida del PC en 0
         elsif rising_edge(Clk) then
-           if_pc_out <= if_pc_in;           --Salida del PC = entrada del PC
+           if_pc_out <= if_mux_out;            --Salida del PC = entrada del PC
         end if;
     end process;
     
     --Adder
-    If_Add: process(Clk)
-    begin
-        if rising_edge(Clk) then
-            if_sig <= (if_pc_out + x"00000004");  --PREGUNTAR PORQUE ERROR
-        else
-            if_sig <= if_pc_out;
-        end if;
-    end process;
+    if_pc_4 <= (if_pc_out + x"00000004");      --Salida Adder = PC + 4
     
     --Mux
-    If_Mux: process (sel_mux, if_sig, mem_branch)
+    If_Mux: process (if_sel_mux, if_pc_4, mem_branch)
     begin      
-        if (sel_mux = '0') then
-            out_mux <= if_sig;              --Salida mux = PC + 4
-        elsif (sel_mux = '1') then
-            out_mux <= mem_branch;          --Salida mux = salto proveniente de etapa mem
+        if (if_sel_mux = '0') then
+            if_mux_out <= if_pc_4;            --Salida mux = PC + 4
+        elsif (if_sel_mux = '1') then
+            if_mux_out <= mem_branch;         --Salida mux = salto proveniente de etapa mem
       end if;   
     end process;
-    
-    if_id_pc_4_in <= if_sig;                --Entrada reg seg Adder = salida Adder
-    
-    if_id_inst_in <= DataOut;               --Entrada reg seg Intrucc mem = salida Intrucc mem
    
-    --Reg seg Adder
---    IF_ID_pc_4: process (Clk, Reset)
---    begin
---        if (Reset = '1') then
---    	   if <= (others => '0');    --Salida del PC en 0
---        elsif rising_edge(Clk) then
---           if_pc_out <= if_pc_in;           --Salida del PC = entrada del PC
---        end if;
---    end process;        	
+    --Reg seg IF/ID
+    Seg_IF_ID: process (Clk, Reset)
+    begin
+        if (Reset = '1') then
+    	   if_id_pc_4_out <= (others => '0');        --Salida del seg en 0
+    	   if_id_inst_out <= (others => '0');        --Salida del seg en 0
+        elsif rising_edge(Clk) then
+           if_id_pc_4_out <= if_pc_4;                --Salida reg seg = PC + 4
+           if_id_inst_out <= I_DataIn;               --Salida reg seg = Instrucc mem
+        end if;
+    end process;        	
         
  
 end processor_arq;
